0df7297dccd9ba62d7e5d9fc0be85b3f53341a2e
lindaweinmer <linda.weinmer@gmail.com>
2016-08-14 17:08:32 +0200

    Styling av kundportal

--- wp-content/themes/wintherperson/lib/setup.php
+++ wp-content/themes/wintherperson/lib/setup.php
@@ -26,9 +26,10 @@ function setup() {
 
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
-  register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'sage')
-  ]);
+  register_nav_menus(array(
+    'primary_navigation' => __('Huvudmeny', 'sage'),
+		'menu_loggedin' => __('Kundportalmeny', 'sage'),
+  ));
 
   // Enable post thumbnails
   // http://codex.wordpress.org/Post_Thumbnails
--- wp-content/themes/wintherperson/template-skapa-anvandare.php
+++ wp-content/themes/wintherperson/template-skapa-anvandare.php
@@ -5,5 +5,5 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'create--update-user'); ?>
+  <?php get_template_part('templates/content', 'create-update-user'); ?>
 <?php endwhile; ?>
